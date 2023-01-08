8a8ecc4c53917f6b18576c370016ba4ca8255688
ludvig <61148058+drtassen@users.noreply.github.com>
2023-01-08 17:48:35 +0100

    sen kveld
    back 2 back

--- Sally/Sally.js
+++ Sally/Sally.js
@@ -1,23 +1,64 @@
-<div class="Line" onclick="ColorChainge">${ordUt[1]}</div>
+function showView(curvball) {
+    document.getElementById('Line1')
+    first - line {
+    font - size: 1.2rem;
+    font - weight: bold;
+    text - decoration: underline;
+}
+}
 
 
 
-fordeling;
-
-Modell
-Controll
-Veiev
-
-Gridd
-
-Linje
-
-
-Kross Gul
-
-Kross Grønd
-
-Kross Rød
+//<div class="Line" onclick="ColorChainge">${ordUt[1]}</div>
+//
+//
+//
+//fordeling;
+//
+//
+//Gridd
+//
+//Linje
+//
+//
+//Kross Gul
+//
+//Kross Grønd
+//
+//Kross Rød
+//
+//Modell;
+//
+//14 linjer: 
+//{
+//
+//    Linje 1: kross screen ::  ? 1
+//    Linje :2
+//    Linje 3
+//    Linje 4
+//    Linje 5
+//    Linje 6
+//    Linje 7
+//    Linje 8
+//    Linje 9
+//    Linje 10
+//    Linje 11
+//    Linje 12
+//    Linje 13
+//    Linje 14
+//}
+//
+//Controll:
+//
+//Bevegelse på linjene
+//Plaserin av krysnig
+//
+//
+//
+//Veiev:
+//Plaserin av krysnig
+//synelig strek 
+//klassifisering av krosning
 
 
 
